b82c1817ed0883d114de7ed621c34d2a4ec1c3a0
anan <anan@iu.edu>
2023-04-25 14:47:55 -0400

    update twilio

--- oop-project-backend/chatroom/src/main/java/oop/project/chatroom/ChatroomApplication.java
+++ oop-project-backend/chatroom/src/main/java/oop/project/chatroom/ChatroomApplication.java
@@ -3,10 +3,12 @@ package oop.project.chatroom;
 import jakarta.validation.Valid;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @SpringBootApplication
+@CrossOrigin(origins = "https://322-frontend-jyl8.vercel.app")
 public class ChatroomApplication {
 
 	public static void main(String[] args) {
--- oop-project-backend/chatroom/src/main/java/oop/project/chatroom/Controller/GreetingController.java
+++ oop-project-backend/chatroom/src/main/java/oop/project/chatroom/Controller/GreetingController.java
@@ -1,11 +1,13 @@
 package oop.project.chatroom.Controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
+@CrossOrigin(origins = "https://322-frontend-jyl8.vercel.app")
 public class GreetingController {
     //Get https:localhost:8080
     @GetMapping
